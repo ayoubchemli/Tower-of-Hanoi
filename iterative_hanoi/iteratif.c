d47608101c807b2c0bd72231a70ffe1d1e28b054
wanesaf <safwanesouhil.dendani@etu.usthb.dz>
2024-03-23 17:55:08 +0100

    algorithme itératif

--- iterative_hanoi/iteratif.c
+++ iterative_hanoi/iteratif.c
@@ -1,57 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
+clock_t start,end;
+
+//la déclaration de la structure pile 
 typedef struct {
     int *pile;
     int sommet;
-} PileEntiers;
+} Pile;
 
-void initPile(PileEntiers *p, int n) {
+// les fonctions élémentaires 
+void initPile(Pile *p, int n) {
     p->pile = (int*) malloc(n * sizeof(int));
     p->sommet = -1;
 }
 
-int vide(PileEntiers *p) {
+int vide(Pile *p) {
     return p->sommet == -1;
 }
 
-int sommet(PileEntiers *p) {
+int sommet(Pile *p) {
     return p->pile[p->sommet];
 }
 
-void empiler(PileEntiers *p, int x) {
+void empiler(Pile *p, int x) {
     p->pile[++(p->sommet)] = x;
 }
 
-int depiler(PileEntiers *p) {
+int depiler(Pile *p) {
     return p->pile[(p->sommet)--];
 }
 
-void afficherPile(PileEntiers *p) {
+void afficherPile(Pile *p) {
     for(int i = 0; i <= p->sommet ; i++) {
         printf("%d ", p->pile[i]);
     }
     printf("\n");
 }
 
-
-void remplir(PileEntiers *p, int n) {
+//remplissage d'un piquet
+void remplir(Pile *p, int n) {
     for(int i = n; i >= 1; i--) {
         empiler(p, i);
     }
 }
 
-void afficherHanoi(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3) {
-    printf("P1 : ");
+void afficherPiquets(Pile *p1, Pile *p2, Pile *p3) {
+    printf("A : ");
     afficherPile(p1);
-    printf("P2 : ");
+    printf("B : ");
     afficherPile(p2);
-    printf("P3 : ");
+    printf("C : ");
     afficherPile(p3);
 }
 
-void resolutionIteratif(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3, int n) {
+//la resolution des tours d'hanoi 
+void resolutionIteratif(Pile *p1, Pile *p2, Pile *p3, int n) {
    int indic = 0;
 
     // Parité de n
@@ -62,7 +68,7 @@ void resolutionIteratif(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3, int n
     }
 
     // Calcul du nb de déplacement min
-    int deplacementMin = (int) pow(2, n) - 1;
+    int deplacementMin = (int) pow(2, n) - 1;//le nombre de déplacement qui seront effectués (2^n-1)
 
     printf("-------------------\n");
 
@@ -70,21 +76,21 @@ void resolutionIteratif(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3, int n
         // n Paire
         if (indic == 1) {
             if (i % 3 == 0) {
-                // Entre P1 et P2 (vers la droite)
+    
                 if (!vide(p1) && (vide(p2) || sommet(p1) < sommet(p2))) {
                     empiler(p2, depiler(p1));
                 } else {
                     empiler(p1, depiler(p2));
                 }
             } else if (i % 3 == 1) {
-                // Entre P1 et P3 (vers la droite)
+                
                 if (!vide(p1) && (vide(p3) || sommet(p1) < sommet(p3))) {
                     empiler(p3, depiler(p1));
                 } else {
                     empiler(p1, depiler(p3));
                 }
             } else {
-                // Entre P2 et P3
+                
                 if (!vide(p2) && (vide(p3) || sommet(p2) < sommet(p3))) {
                     empiler(p3, depiler(p2));
                 } else {
@@ -95,21 +101,21 @@ void resolutionIteratif(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3, int n
         // n Impair
         else {
             if (i % 3 == 0) {
-                // Entre P1 et P3
+                
                 if (!vide(p1) && (vide(p3) || sommet(p1) < sommet(p3))) {
                     empiler(p3, depiler(p1));
                 } else {
                     empiler(p1, depiler(p3));
                 }
             } else if (i % 3 == 1) {
-                // Entre P1 et P2 (vers la gauche)
+                
                 if (!vide(p1) && (vide(p2) || sommet(p1) < sommet(p2))) {
                     empiler(p2, depiler(p1));
                 } else {
                     empiler(p1, depiler(p2));
                 }
             } else {
-                // Entre P2 et P3
+                
                 if (!vide(p3) && (vide(p2) || sommet(p3) < sommet(p2))) {
                     empiler(p2, depiler(p3));
                 } else {
@@ -118,38 +124,41 @@ void resolutionIteratif(PileEntiers *p1, PileEntiers *p2, PileEntiers *p3, int n
             }
         }
         printf("déplacement numéro :  %d \n", i+1); 
-        afficherHanoi(p1, p2, p3);
+        afficherPiquets(p1, p2, p3);
         printf("-------------------\n");
     }
 }
 
 int main() {
     int n;
-    
     do
     {
     printf("Saisir le nombre de disques : ");
     scanf("%d", &n);   
     } while (n<=0);
     
-
-    PileEntiers p1, p2, p3;
+    //l'initialisation des piles
+    Pile p1, p2, p3;
     initPile(&p1, n);
     initPile(&p2, n);
     initPile(&p3, n);
 
 
-    remplir(&p1, n);// on fait remplir le premier piquer par les disques 
+    remplir(&p1, n);// remplissage du premier piquet par les n disques 
     printf("-------------------\n");
     printf("l'état des piquets avant le déplacement : \n");
-     printf("-------------------\n");
-    afficherHanoi(&p1,&p2,&p3);
+    printf("-------------------\n");
+    afficherPiquets(&p1,&p2,&p3);
     printf("-------------------\n");
     printf("l'état des piquets aprés le déplacement : \n");
-    resolutionIteratif(&p1, &p2, &p3, n);
+    // le plus grand disque a comme numéro n , aprés le disque le moins grand c'est  n-1 et ainsi de suite ...
     
-
-    // le plus grand disque a comme numéro 1 , aprés le disque le moins grand c'est  2 et ainsi de suite ...
+    //pour calculer le temp de la résolution 
+    start=clock();   
+    resolutionIteratif(&p1, &p2, &p3, n);
+    end=clock();
+    float time=(float)(end-start)/CLOCKS_PER_SEC;
+    printf("\t\tLe temps d'execution est : %f\n\n",time);
     return 0;
 
 }
